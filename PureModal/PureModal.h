200e47d8cce2bbea473c63c6e3e953bd6260cc5e
iMoeNya <sym9688@outlook.com>
2017-08-23 09:25:03 +0800

    Initial Commit

---
+++ PureModal/PureModal.h
@@ -0,0 +1,19 @@
+//
+//  PureModal.h
+//  PureModal
+//
+//  Created by 孙一萌 on 2017/8/23.
+//  Copyright © 2017年 iMoeNya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PureModal.
+FOUNDATION_EXPORT double PureModalVersionNumber;
+
+//! Project version string for PureModal.
+FOUNDATION_EXPORT const unsigned char PureModalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureModal/PublicHeader.h>
+
+
